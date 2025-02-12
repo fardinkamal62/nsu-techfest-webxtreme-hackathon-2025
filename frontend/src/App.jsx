4ae16cf7542b322c76c0424ae2176e1b30a6b7af
rafiulomar <rafiulomar2022@gmail.com>
2025-02-12 10:29:46 +0600

    react router setup, and nav and login compoent demo added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
-import './App.css'
+import { Outlet } from 'react-router-dom'
+import Nav from './components/Nav/Nav'
 
 function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>    
-    <button className="btn btn-active">Default</button>
+      <div className="min-h-screen bg-cover bg-center">
+      <Nav></Nav>
+      <Outlet />
+    </div>
     </>
   )
 }
